285e76c88a69dcbe70d411db8c092566d8dfa3be
Rodrigo Interliche <kormaibr@gmail.com>
2020-06-04 09:34:40 -0300

    select congregation on sign up

--- account/forms.py
+++ account/forms.py
@@ -3,29 +3,34 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
 from django.utils.translation import ugettext, ugettext_lazy as _
 
-from account.models import JWAdminUser
+from account.models import JWAdminUser, JWAdminCongregation
 
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=255, label="Email")
     email.widget.attrs.update(
-        {"class": "form-control", "placeholder": "Digite seu email"})
+        {"class": "form-control", "placeholder": "Digite seu email", "autocomplete": "off"})
 
     first_name = forms.CharField(max_length=50, label="Primeiro nome")
     first_name.widget.attrs.update(
-        {"class": "form-control", "placeholder": "Digite seu primeiro nome"})
+        {"class": "form-control", "placeholder": "Digite seu primeiro nome", "autocomplete": "off"})
 
     last_name = forms.CharField(max_length=50, label="Último nome")
     last_name.widget.attrs.update(
-        {"class": "form-control", "placeholder": "Digite seu último nome"})
+        {"class": "form-control", "placeholder": "Digite seu último nome", "autocomplete": "off"})
+
+    congregation = forms.ChoiceField(label="Congregação", choices=[(0, 'Selecione sua congregação')] + [(
+        congregation.id, congregation.name) for congregation in JWAdminCongregation.objects.all()])
+    congregation.widget.attrs.update(
+        {"class": "form-control"})
 
     password1 = forms.CharField(label="Senha", widget=forms.PasswordInput)
     password1.widget.attrs.update(
-        {"class": "form-control", "placeholder": "Digite sua senha"})
+        {"class": "form-control", "placeholder": "Digite sua senha", "autocomplete": "off"})
 
     password2 = forms.CharField(label="Digite novamente sua senha", widget=forms.PasswordInput)
     password2.widget.attrs.update(
-        {"class": "form-control", "placeholder": "Confirme sua senha"})
+        {"class": "form-control", "placeholder": "Confirme sua senha", "autocomplete": "off"})
 
     class Meta:
         model = JWAdminUser
@@ -33,8 +38,14 @@ class RegistrationForm(UserCreationForm):
             "email",
             "first_name",
             "last_name",
+            "congregation",
             "password1",
             "password2",)
+
+    def clean_congregation(self):
+        congregation_id = self.cleaned_data['congregation']
+        congregation = JWAdminCongregation.objects.get(id=congregation_id)
+        return congregation
 
 
 class AuthenticationForm(forms.ModelForm):
