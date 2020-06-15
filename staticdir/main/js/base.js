61c0a1bfb467c806653c3a295e4e717e8af2b83e
Rodrigo Interliche <kormaibr@gmail.com>
2020-06-15 14:57:34 -0300

    Delete service occurrence fix

--- staticdir/main/js/base.js
+++ staticdir/main/js/base.js
@@ -1,18 +1,31 @@
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+var csrftoken = getCookie('csrftoken');
+
 function delete_service_occurrence(id) {
     $.ajax({
         type: "DELETE",
-        headers: {
-            "X-CSRFToken": "{{ csrf_token }}"
-        },
-        url: "{{ HTTP_HOST }}/api/service_ocurrence/" + id + "/",
-        data: {
-            csrfmiddlewaretoken: "{{ csrf_token }}"
-        },
+        url: "/api/service_ocurrence/" + id + "/",
         contentType: 'application/json',
-        beforeSend: function () {
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
             return confirm("Tem certeza que deseja excluir a saída de campo?");
         },
-        success: function (data, textStatus) {
+        success: function () {
             alert("Saída de campo excluído com sucesso!");
             location.reload();
         },
